d142449563b2de0e0b891a040092a0392ab61d30
Milosska <110258611+Milosska@users.noreply.github.com>
2023-05-12 00:59:22 +0300

    basic service functions and controllers

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,40 @@
-const express = require('express')
+const express = require("express");
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  updateContact,
+  removeContact,
+} = require("../../models/contacts");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  const contacts = await listContacts();
+  res.json(contacts);
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  const { contactId } = req.params;
+  const contact = await getContactById(contactId);
+  res.json(contact);
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  const newContact = await addContact(req.body);
+  res.status(201).json(newContact);
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put("/:contactId", async (req, res, next) => {
+  const { contactId } = req.params;
+  const editedContact = await updateContact(contactId, req.body);
+  res.json(editedContact);
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res, next) => {
+  const { contactId } = req.params;
+  await removeContact(contactId);
+  res.json({ message: "contact deleted" });
+});
 
-module.exports = router
+module.exports = router;
