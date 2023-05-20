3cd4648eed3374edd753395d91e76162a102cee1
Milosska <110258611+Milosska@users.noreply.github.com>
2023-05-20 20:45:44 +0300

    hw 03 finished

--- models/contacts.js
+++ models/contacts.js
@@ -1,64 +1,30 @@
-const fs = require("fs/promises");
-const path = require("path");
-const crypto = require("crypto");
-const { HttpError } = require("../utils/HttpError");
+const { Schema, model } = require("mongoose");
+const { handleMongooseError } = require("../utils/MongooseError");
 
-const contactsPath = path.join(__dirname, "contacts.json");
+const contactsSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
 
-const listContacts = async () => {
-  const contacts = await fs.readFile(contactsPath);
-  return JSON.parse(contacts);
-};
+contactsSchema.post("save", handleMongooseError);
 
-const getContactById = async (contactId) => {
-  const contactsList = await listContacts();
-  const searchedContact = contactsList.find(
-    (contact) => contact.id === contactId
-  );
-  if (!searchedContact) {
-    throw new HttpError(404, "Not found");
-  }
-  return searchedContact || null;
-};
-
-const addContact = async (body) => {
-  const contactsList = await listContacts();
-  const newContact = { id: crypto.randomUUID(), ...body };
-  contactsList.push(newContact);
-  fs.writeFile(contactsPath, JSON.stringify(contactsList, null, 2));
-  return newContact;
-};
-
-const updateContact = async (contactId, body) => {
-  const contactsList = await listContacts();
-  const editedContact = contactsList.find(
-    (contact) => contact.id === contactId
-  );
-  if (!editedContact) {
-    throw new HttpError(404, "Not found");
-  }
-  editedContact.name = body.name;
-  editedContact.email = body.email;
-  editedContact.phone = body.phone;
-  fs.writeFile(contactsPath, JSON.stringify(contactsList, null, 2));
-  return editedContact;
-};
-
-const removeContact = async (contactId) => {
-  const contactsList = await listContacts();
-  const index = contactsList.findIndex((contact) => contact.id === contactId);
-  if (index === -1) {
-    throw new HttpError(404, "Not found");
-  }
-  const [removedContact] = contactsList.splice(index, 1);
-  await fs.writeFile(contactsPath, JSON.stringify(contactsList, null, 2));
-  return removedContact;
-};
+const Contact = model("contact", contactsSchema);
 
 module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
+  Contact,
 };
